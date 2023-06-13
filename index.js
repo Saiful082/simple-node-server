1adb8f8a3da4d554bbff2ab058be4e995ed25141
Saiful082 <smsaifulislam082@gmail.com>
2023-06-14 00:35:35 +0600

    mongobd database setup

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) =>{
+    res.send('Simple Node Server Running');
+});
+
+app.use(cors());
+app.use(express.json());
+
+const users = [
+    { id: 1, name: 'sakib', email: 'sakib@gmail.com' },
+    { id: 2, name: 'rakib', email: 'rakib@gmail.com' },
+    { id: 3, name: 'korim', email: 'korim@gmail.com' }
+];
+
+
+
+
+const uri = "mongodb+srv://saiful082:K3YeKm94dYiauQ2o@cluster0.m92agsd.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("simpleNode").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+   console.log('database connected');
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/users', (req, res) =>{
+    if(req.query.name){
+        const search = req.query.name;
+        const filtered = users.filter(user => user.name.toLocaleLowerCase().indexOf(search))
+        res.req(filtered);
+    }   
+
+    else {
+        res.send(users);
+    }
+   
+})
+
+
+
+
+app.post('/users', (req, res) => {
+    console.log('Post Api called');
+    const user = req.body;
+    user.id = users.length + 1;
+    users.push(user);
+    console.log(user);
+    res.send(user);
+})
+
+
+app.listen(port, () => {
+    console.log(`Simple not server running on port. ${port}`);
+})
